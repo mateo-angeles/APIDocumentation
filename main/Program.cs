4eb1f7f2a0e1208921ea43ed5f06971628ff2f0f
mateoangeles-bsi <mateo.angeles1999@hotmail.com>
2025-08-19 11:45:51 -0500

    Initial commit
    Initial commit with all working files.

---
+++ main/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// This is a simple C# program to demonstrate API documentation with Github pages.
+/// </summary> 
+
+/// <summary>
+/// This is a simple string variable.
+/// </summary>
+string foo = "Hello, World!";
+
+/// <summary>
+/// This is a console write line operation.
+/// </summary>
+Console.WriteLine(foo);
