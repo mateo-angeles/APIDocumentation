7544a038d0c3e262d07d9243ed0b8bae63d4b181
mateo-angeles <mateo.angeles1999@hotmail.com>
2025-09-02 13:46:48 -0400

    Added GetSnippet Function This function gets a snippet of code with radius of 40 lines above and below the specified documentation. It does this by  finding the documentation file (not including wild card characters like "\" and extensions), reading the content into an array, and joining the content within a 40 line radius into a single line.

--- apidocumentation.ps1
+++ apidocumentation.ps1
@@ -17,3 +17,13 @@ New-Item -ItemType Directory -Force -Path $outputDirectory | Out-Null
 
 function SafeName([string]$string) { $s -replace '[^A-Za-z0-9_.-]+','_' }
 
+function GetSnippet{
+  param([string]$filePath, [int]$startLine, [int]$radius = 40)
+  if (-not $filePath) {return ""}
+  $path = Resolve-Path -LiteralPath $filePath -ErrorAction SilentlyContinue
+  if (-not $path) { return "" }
+  $lines = Get-Content -LiteralPath $path -Encoding UTF8
+  $indexZero = [Math]::Max(0, $startLine - 1 - $radius)
+  $indexOne = [Math]::Min($lines.Count - 1, $startLine - 1 + $radius)
+  ($lines[$indexZero, $indexOne] -join "`n")
+}
