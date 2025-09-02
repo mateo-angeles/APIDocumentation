5dc92647a9d0ce4795d388cc94ae30d3f991e72d
mateo-angeles <mateo.angeles1999@hotmail.com>
2025-09-02 13:21:17 -0400

    Adding Various PowerShell Commands Get-Location gets the path of the working directory. Join-Path joins the path of the reference folder and the desired path. New-Item creates a new item.

--- apidocumentation.ps1
+++ apidocumentation.ps1
@@ -2,3 +2,15 @@ docfx.exe metadata .\docfxWindows.json
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+# Check if running on powershell 7+
+if (-not (Get-Command ConvertFrom-Yaml -ErrorAction SilentlyContinue))
+{
+  throw "This step required PowerShell 7+ (ConvertFrom-Yaml). The GitHub 
+  'windows-latest' runner includes pwsh."
+}
+
+$repo = Get-Location
+$apiDirectory = Join-Path $repo "api"
+$outputDirectory = Join-Path $repo "apidocs"
+New-Item -ItemType Directory -Force -Path $outputDirectory | Out-Null
